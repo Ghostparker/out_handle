8160ae8dcf934c392928e4566853cb12d2a5bc5a
Ghostparker <ghostparker@163.com>
2020-11-23 17:28:29 +0800

    first commit

--- parse_json.py
+++ parse_json.py
@@ -1,15 +1,36 @@
-import sys,os,cv2
+import sys,os,cv2,json
 import os.path as osp
 
+
+
+
+def crop_only_onecar(resjson, save_dir):
+    try:
+
+
+    except:
+        raise
+
 def main():
-    if(len(sys.argv) <= 3)
+    if(len(sys.argv) <= 3):
         print('Please input jsonfile , picdir ')
         exit(1)
     else:
         jsonfile = sys.argv[1]
         pic_dir = sys.argv[2]
+        save_dir = '/root/cutpic/{}'.format(pic_dir.split('/')[-1])
 
-    print(jsonfile,pic_dir)
+    if(osp.exists(save_dir) == False):
+        os.makedirs(save_dir)
+
+    for line in open(jsonfile):
+        resjson = json.loads(line.strip())
+        print(resjson)
+
+        exit(1)
+
+
+
 
 
 

68fe0c6c2d1d2e3a06d7f5c9579bbd2cb472dd34
Ghostparker <ghostparker@163.com>
2020-11-23 17:22:56 +0800

    first commit

---
+++ parse_json.py
@@ -0,0 +1,17 @@
+import sys,os,cv2
+import os.path as osp
+
+def main():
+    if(len(sys.argv) <= 3)
+        print('Please input jsonfile , picdir ')
+        exit(1)
+    else:
+        jsonfile = sys.argv[1]
+        pic_dir = sys.argv[2]
+
+    print(jsonfile,pic_dir)
+
+
+
+if __name__ == '__main__':
+    main()
